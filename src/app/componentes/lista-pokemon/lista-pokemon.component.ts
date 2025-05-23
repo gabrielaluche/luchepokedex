4d38dce8abf41cf2be44644d2e5e8c862090942d
gabrielaluche <gabrielalimagomes@gmail.com>
2025-05-23 10:02:03 -0300

    Ajuste nas imagens

--- src/app/componentes/lista-pokemon/lista-pokemon.component.ts
+++ src/app/componentes/lista-pokemon/lista-pokemon.component.ts
@@ -11,34 +11,48 @@ import { Pokemon } from 'src/app/shared/model/pokemon'; // Importa o modelo Poke
   styleUrls: ['./lista-pokemon.component.scss'], // Caminho do CSS
 })
 export class ListaPokemonComponent implements OnInit {
-    // Classe do componente que implementa o ciclo de vida OnInit
-    // Lista que será exibida na tela, contendo os Pokémons filtrados
-    listaPokemon: Pokemon[] = [];
-    // String que armazena o texto digitado no campo de filtro
-    filtro = '';
-    // Lista geral com todos os Pokémons retornados pela API
-    listaGeral: Pokemon[] = [];
-    // Lista com os Pokémons mapeados para um modelo mais adequado (classe Pokemon)
-    listaMap: Pokemon[] = [];
-    // Injeção de dependências via construtor
-    constructor(
-        private pokedexService: PokedexService, // Serviço responsável por fazer requisições à API
-        private mapper: PokeapiMapper // Classe responsável por transformar (mapear) os dados da API para modelos utilizados na aplicação
-    ) {}
+  // Classe do componente que implementa o ciclo de vida OnInit
+  // Lista que será exibida na tela, contendo os Pokémons filtrados
+  listaPokemon: Pokemon[] = [];
+  // String que armazena o texto digitado no campo de filtro
+  filtro = '';
+  // Lista geral com todos os Pokémons retornados pela API
+  listaGeral: Pokemon[] = [];
+  // Lista com os Pokémons mapeados para um modelo mais adequado (classe Pokemon)
+  listaMap: Pokemon[] = [];
+  // Injeção de dependências via construtor
+  constructor(
+    private pokedexService: PokedexService, // Serviço responsável por fazer requisições à API
+    private mapper: PokeapiMapper // Classe responsável por transformar (mapear) os dados da API para modelos utilizados na aplicação
+  ) {}
 
-    // Método que filtra a lista de Pokémons com base no evento (texto digitado)
-    filtraPokemon(event: any): void {
-      this.listaPokemon = this.listaGeral.filter((item) =>
-        // Filtra pelo nome que inclui o texto digitado
-        item.nome.includes(event as string)
-      );
-    }
-    // Método do ciclo de vida que é executado automaticamente quando o componente é inicializado
-    ngOnInit(): void {
+  // Método que filtra a lista de Pokémons com base no evento (texto digitado)
+  filtraPokemon(event: any): void {
+    this.listaPokemon = this.listaGeral.filter((item) =>
+      // Filtra pelo nome que inclui o texto digitado
+      item.nome.includes(event as string)
+    );
+  }
+
+  // Método do ciclo de vida que é executado automaticamente quando o componente é inicializado
+  ngOnInit(): void {
     // Chamada ao serviço para buscar a lista de Pokémons
-      this.pokedexService.getListaPokemon().subscribe((response) => { // Faz a requisição à API e obtém a resposta
+    this.pokedexService.getListaPokemon().subscribe((response) => {
+      // Faz a requisição à API e obtém a resposta
       this.listaPokemon = this.mapper.mapFromDTO(response.results); // Mapeia os dados da API para o modelo Pokemon
       this.listaGeral = this.listaPokemon; // Armazena a lista geral de Pokémons
     });
+  }
+
+  setFallbackImage(event: Event) {
+    const imgElement = event.target as HTMLImageElement;
+    const currentSrc = imgElement.src;
+
+    const idMatch = currentSrc.match(/\/(\d+)\.gif$/);
+
+    if (idMatch) {
+      const id = idMatch[1];
+      imgElement.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${id}.png`;
+    }
   }
 }
